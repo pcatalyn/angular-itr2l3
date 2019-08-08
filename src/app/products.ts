f297ecc56affafc08d8b7df9eb4b18d984186dfd
pcatalyn <pohribcatalyn@gmail.com>
2019-08-08 15:55:55 +0300

    3 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,38 +1,41 @@
 export const products = [
   {
     name: 'IMC0001',
-    price: 799,
-    description: 'Oracle Database Upgrade Workshop'
+    duration: 2,
+    shortdescription: 'Oracle Database Upgrade Workshop',
+    fulldescription:'Upgrading to latest of Oracle Database provides the latest in efficient, reliable, secure data management for mission-critical on-line transaction processing applications, query-intensive data warehouses, and cloud applications. The right planning, preparation, and upgrade steps will make the upgrade process simpler, faster, and more predictable from start to finish. Oracles database utilities include Data Pump, SQL*Loader, Transportable Tablespaces, External Tables, and the original exp/imp utilities. These are all ways of moving data into or between Oracle databases, including cross-version and cross-platform migration.'
   },
   {
     name: 'IMC0002',
-    price: 699,
-    description: 'Architectural Principles and Best Practices for Adopting Oracle Cloud'
+    duration: 2,
+    description: 'Architectural Principles and Best Practices for Adopting Oracle Cloud',
+    fulldescription:"Principles and Best Practices for Cloud Adoption, General considerations, Oracle Cloud Overview & Strategy, deciding on the cloud approach, describing the simplified workflow for getting on board the Cloud,overview of roles in the journey to the Cloud. This training will equip solution architects with the skills required to embark on a journey of assessing their current on-premises environment, defining the IT and non-IT goals for migrating to the cloud, understanding the operational targets, deciding the cloud topology, planning the migration tasks, and conducting the migration process. This involves answering a series of specific questions that will progressively help students narrow down the range of cloud deployment options and develop a migration strategy that's built on an optimal blend of Oracle Cloud service offerings."
   },
   {
     name: 'IMC0005',
-    price: 699,
+    duration: 1.5,
     description: 'Oracle Security and Compliance for Oracle Database'
   },
   {
     name: 'IMC0006',
-    price: 699,
+    duration: 3,
     description: 'Revolutionizing Development with Oracle Cloud PaaS Services / out of date (could)'
   },
   {
     name: 'IMC0009.A',
-    price: 699,
+    duration: 1,
     description: 'Oracle Digital Assistant and Chatbots'
   },
 {
     name: 'IMC0009.B',
-    price: 699,
+    duration: 2,
     description: 'Oracle Digital Assistant and Chatbots'
   },
   {
     name: 'IMC0009.C',
-    price: 299,
-    description: 'Oracle Digital Assistant and Chatbots'
+    duration: 3,
+    shortdescription: 'Oracle Digital Assistant and Chatbots',
+    fulldescription:'adadsasdadadadasssssssssssssssss'
   }
 ];
 
