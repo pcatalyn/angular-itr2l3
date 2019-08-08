96c5ce91eae575a31b9ecbc95ab26082e22a6b20
pcatalyn <pohribcatalyn@gmail.com>
2019-08-08 15:26:43 +0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    name: 'IMC0001',
+    price: 799,
+    description: 'Oracle Database Upgrade Workshop'
+  },
+  {
+    name: 'IMC0002',
+    price: 699,
+    description: 'Architectural Principles and Best Practices for Adopting Oracle Cloud'
+  },
+  {
+    name: 'IMC0005',
+    price: 699,
+    description: 'Oracle Security and Compliance for Oracle Database'
+  },
+  {
+    name: 'IMC0006',
+    price: 699,
+    description: 'Revolutionizing Development with Oracle Cloud PaaS Services / out of date (could)'
+  },
+  {
+    name: 'IMC0009.A',
+    price: 699,
+    description: 'Oracle Digital Assistant and Chatbots'
+  },
+{
+    name: 'IMC0009.B',
+    price: 699,
+    description: 'Oracle Digital Assistant and Chatbots'
+  },
+  {
+    name: 'IMC0009.C',
+    price: 299,
+    description: 'Oracle Digital Assistant and Chatbots'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
